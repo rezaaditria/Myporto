7f79d0e3da71714bdf8b0cf4d33a3c26fbeedc62
rezaaditria <rezaaditria1@gmail.com>
2023-12-27 16:09:09 +0700

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
       <Navbar/>
       {children}
-      <Bubble/>
+      {/* <Bubble/> */}
       </body>
     </html>
   )
