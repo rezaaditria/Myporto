7f6a3ad9f4ef8ece4309e25774e88999d456cebf
rezaaditria <rezaaditria1@gmail.com>
2023-12-29 00:55:17 +0700

    akh

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Navbar from '@/components/Navbar/navigasi'
+import Navbar from '@/components/Navbar/navbar'
 import Bubble from '@/components/Bubble/Bubble'
 import React from "react";
 
